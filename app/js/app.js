6afb55ce1a5500efd35d0f3d30d708a365e00d0c
Stephen Theogene <stheogene@feengo.com>
2017-07-18 15:58:56 -0400

    Added required calls for slider

--- app/js/app.js
+++ app/js/app.js
@@ -2,3 +2,7 @@ console.log('App loaded');
 
 require('../styles/app.scss');
 require('../styles/font-awesome.min.css');
+require('custom.js');
+require('slick.min.js');
+require('../styles/slick.css');
+require('../styles/slick-theme.css');
