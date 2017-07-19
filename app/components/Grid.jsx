1384e4197646d57f6e1fcfb3818ff2cf79f5447d
Stephen Theogene <stheogene@feengo.com>
2017-07-18 21:51:15 -0400

    Responsive queries

--- app/components/Grid.jsx
+++ app/components/Grid.jsx
@@ -12,31 +12,41 @@ var Grid = (props) => {
                     <p>Presented by <strong>Lorem Ipsum</strong></p>
                 </div>
                 <div className="grid-block" id="block-2">
-                    <img src={require('../images/block-2.jpg')} alt="Block 2" className="grid-thumb"/>
+                    <div className="grid-thumb">
+                        <img src={require('../images/block-2.jpg')} alt="Block 2"/>
+                    </div>
                     <small className="grid-date">February 2 2017</small>
                     <h2><a>Sharp bureaux sleepy K-pop carefully curated.</a></h2>
                     <p>Presented by <strong>Lorem Ipsum</strong></p>
                 </div>
                 <div className="grid-block" id="block-3">
-                    <img src={require('../images/block-3.jpg')} alt="Block 3" className="grid-thumb"/>
+                    <div className="grid-thumb">
+                        <img src={require('../images/block-3.jpg')} alt="Block 3"/>
+                    </div>
                     <small className="grid-date">January 27 2017</small>
                     <h2><a>St Moritz uniforms Beams.</a></h2>
                     <p>Presented by <strong>Lorem Ipsum</strong></p>
                 </div>
                 <div className="grid-block" id="block-4">
-                    <img src={require('../images/block-4.jpg')} alt="Block 4" className="grid-thumb"/>
+                    <div className="grid-thumb">
+                        <img src={require('../images/block-4.jpg')} alt="Block 4"/>
+                    </div>
                     <small className="grid-date">January 21 2017</small>
                     <h2><a>Bureaux exquisite delightful carefully curated soft power.</a></h2>
                     <p>Presented by <strong>Lorem Ipsum</strong></p>
                 </div>
                 <div className="grid-block" id="block-5">
-                    <img src={require('../images/block-5.jpg')} alt="Block 5" className="grid-thumb"/>
+                    <div className="grid-thumb">
+                        <img src={require('../images/block-5.jpg')} alt="Block 5"/>
+                    </div>
                     <small className="grid-date">January 18 2017</small>
                     <h2><a>K-pop extraordinary.</a></h2>
                     <p>Presented by <strong>Lorem Ipsum</strong></p>
                 </div>
                 <div className="grid-block" id="block-6">
-                    <img src={require('../images/block-6.jpg')} alt="Block 6" className="grid-thumb"/>
+                    <div className="grid-thumb">
+                        <img src={require('../images/block-6.jpg')} alt="Block 6"/>
+                    </div>
                     <small className="grid-date">February 10 2017</small>
                     <h2><a>Artisanal iconic cutting-edge business class.</a></h2>
                     <p>Presented by <strong>Lorem Ipsum</strong></p>
